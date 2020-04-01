e158ab2a26c460b5cf0b8723501d20ac9ddf8490
beelzebub041 <beelzebub00401@hotmail.com>
2020-04-01 21:57:03 +0800

    [MODIFY]Packet 1. 修改定義

--- Doc/Packet.cs
+++ Doc/Packet.cs
@@ -11,56 +11,110 @@
 
 namespace Packet.ServerToClient
 {
-    class S2C_UserRegisteredResult
+    // ======================= User ======================= //
+    
+    // 使用者註冊結果
+    class UserRegisteredResult
     {
-        /**
-         * 註冊結果
-         * JSON範例
-            {
-	        
-			    "Name":"S2C_UserRegisteredResult",
-			    "Data":
-			    {
-				    "result":1
-			    }
-	        
-            }
-         * 
-         * 0: 註冊成功
-         * 1: 帳號重複
-         * 2: 密碼錯誤
-         * 3: 帳號格式不符
-         */
-        public int Result { get; set; }
+        /*
+        * 0: 註冊成功
+        * 1: 帳號重複
+        * 2: 密碼錯誤
+        * 3: 帳號格式不符
+        */
+        public int result { get; set; }
+    }
+
+    // 使用者登入結果
+    class UserLoginResult
+    {
+        /*
+        * 0: 登入成功
+        * 1: 帳號錯誤
+        * 2: 密碼錯誤
+        */
+        public int result { get; set; }
+    }
+
+    // 使用者登入結果
+    class UserLogoutResult
+    {
+        /*
+        * 0: 登出成功
+        * 1: 登出失敗
+        */
+        public int result { get; set; }
+    }
+
+    // 更新使用者資訊結果
+    class UpdateUserInfoResult
+    {
+        /*
+        * 0: 更新成功
+        * 1: 更新失敗
+        */
+        public int result { get; set; }
     }
 }
 
 namespace Packet.ClientToServer
 {
-    /**
-     * 
-     * 
-     * JSON範例
-        {
-		    "Name":"C2S_UserRegistered",
-		    "Data":
-		    {
-			    "account":"123456@hotmail.com",
-			    "password":"Aa123456",
-			    "checkPassword":"Aa123456"
-		    }
+    
+    // ======================= User ======================= //
 
-	    }
-     */
-    class C2S_UserRegistered
+    /*
+    * 使用者註冊
+    */
+    class UserRegistered
     {
-        // 帳號
-        public string Account { get; set; }
+        // Email
+        public string email { get; set; }
 
         // 密碼
-        public string Password { get; set; }
+        public string password { get; set; }
 
         // 確認密碼
-        public string CheckPassword { get; set; }
+        public string checkPassword { get; set; }
     }
+
+    /*
+    * 使用者登入
+    */
+    class UserLogin
+    {
+        // Email
+        public string email { get; set; }
+
+        // 密碼
+        public string password { get; set; }
+
+    }
+
+    /*
+    * 使用者登出
+    */
+    class UserLogout
+    {
+        // Email
+        public string email { get; set; }
+
+    }
+
+    /*
+    * 更新使用者資訊
+    */
+    class UpsateUserInfo
+    {
+        // 暱稱
+        public string nikeName { get; set; }
+
+        // 生日
+        public string birthday { get; set; }
+
+        // 國家
+        public string country { get; set; }
+
+    }
+
+
 }
