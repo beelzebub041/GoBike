748007ec0aaa0d9645404389c98df1e96b945eed
beelzebub041 <beelzebub00401@hotmail.com>
2020-09-04 22:19:04 +0800

    [ADD]Tools 1. 新增Redis Hash 轉換工具

---
+++ RideService/RideService/Tools/RedisHashTransfer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StackExchange.Redis;
+using System.Reflection;
+
+namespace Tools.RedisHashTransfer
+{
+    class RedisHashTransfer
+    {
+        public RedisHashTransfer()
+        {
+
+        }
+
+        // 將物件轉換成 Hash Entry Array
+        public HashEntry[] TransToHashEntryArray(object hashObject)
+        {
+            PropertyInfo[] properties = hashObject.GetType().GetProperties();
+
+            return properties.Select(property => new HashEntry(property.Name, property.GetValue(hashObject).ToString())).ToArray();
+        }
+    }
+}
